1ca99b0ea50d1e976dfc0737c27f3189002360f9
HenryB2007 <hbegeman2@huskers.unl.edu>
2025-08-28 17:23:00 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Henry Begeman
+ * Date: 08/28/2025
+ *
+ * program to print my name
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char **argv) {
+
+    printf("Henry Begeaman)
+}
